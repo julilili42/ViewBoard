7c2cbccfbaef4cf143b06240b1d92d59aa61a5d9
ronny <ronny.goettler@student.uni-tuebingen.de>
2025-05-31 17:17:15 +0200

    [config] added jbcrypt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
 }
 
 dependencies {
-
+    // default //
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -56,4 +56,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // project specific //
+    implementation("org.mindrot:jbcrypt:0.4")
 }
