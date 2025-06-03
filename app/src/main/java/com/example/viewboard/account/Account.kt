86d8f691a07092576b651b834cfe354bf72b6c86
ronny <ronny.goettler@student.uni-tuebingen.de>
2025-06-03 15:56:02 +0200

    [doc] added class documentation

--- app/src/main/java/com/example/viewboard/account/Account.kt
+++ app/src/main/java/com/example/viewboard/account/Account.kt
@@ -2,8 +2,13 @@ package com.example.viewboard.account
 
 import org.mindrot.jbcrypt.BCrypt
 
+/**
+ * @property name the name which is also immutable
+ * @property eMail the E-Mail
+ * @property password the password in plain text
+ */
 class Account (
-    private val name: String,       // immutable //
+    private val name: String,
     private val eMail: String,
     private val password: String
 ) {
@@ -11,27 +16,54 @@ class Account (
     private var m_eMail: String = eMail
     private var m_pwHash: String = initPassword(password)
 
+    /**
+     * Get the name
+     *
+     * @return the name
+     */
     public fun getName() : String {
         return m_name
     }
 
+    /**
+     * Get the E-Mail
+     *
+     * @return the E-Mail
+     */
     public fun getEMail() : String {
         return m_eMail
     }
 
+    /**
+     * Set the E-Mail
+     *
+     * @param eMail the E-Mail
+     */
     public fun setEMail(eMail: String) {
         m_eMail = eMail
     }
 
-    private fun initPassword(password: String) : String {
-        return BCrypt.hashpw(password, BCrypt.gensalt())
-    }
-
+    /**
+     * Set the password
+     *
+     * @param password the password in plain text
+     */
     public fun setPassword(password: String) {
         m_pwHash = BCrypt.hashpw(password, BCrypt.gensalt())
     }
 
+    /**
+     * Checks if the password matches
+     *
+     * @param password the password in plain text
+     * @return true if the password matches, otherwise false
+     */
     public fun checkPassword(password: String) : Boolean {
         return BCrypt.checkpw(password, m_pwHash)
+    }
+
+    // only for internal initialization usage //
+    private fun initPassword(password: String) : String {
+        return BCrypt.hashpw(password, BCrypt.gensalt())
     }
 }
--- app/src/main/java/com/example/viewboard/issue/Issue.kt
+++ app/src/main/java/com/example/viewboard/issue/Issue.kt
@@ -1,12 +1,20 @@
 package com.example.viewboard.issue
 
+/**
+ * @property ID the ID which is also immutable, use ID=0 if it is not synced with any higher abstraction storage
+ * @property title the title
+ * @property desc the description, could be null
+ * @property creator the user who created the issue, who is also immutable
+ * @property assignments the users assigned to the issue, max assignments = 255
+ * @property labels the labels assigned to the issue, max labels = 255
+ */
 class Issue (
-    private val ID: UInt,                       // immutable, 0 = not synced //
+    private val ID: UInt,
     private val title: String,
-    private val desc: String?,                  // could be null //
-    private val creator: String,                // immutable //
-    private val assignments: ArrayList<String>, // max assignments = 255 //
-    private val labels: ArrayList<String>       // max labels      = 255 //
+    private val desc: String?,
+    private val creator: String,
+    private val assignments: ArrayList<String>,
+    private val labels: ArrayList<String>
 ) {
     private val m_ID: UInt = ID
     private var m_title: String = title
@@ -15,58 +23,124 @@ class Issue (
     private val m_assignments: ArrayList<String> = assignments
     private val m_labels: ArrayList<String> = labels
 
+    /**
+     * Get the ID
+     *
+     * @return the ID
+     */
     public fun getID() : UInt {
         return m_ID
     }
 
+    /**
+     * Get the title
+     *
+     * @return the title
+     */
     public fun getTitle() : String {
         return m_title
     }
 
+    /**
+     * Update the title
+     *
+     * @param title the new title
+     */
     public fun updateTitle(title: String) {
         m_title = title
     }
 
+    /**
+     * Get the description
+     *
+     * @return the description
+     */
     public fun getDesc() : String? {
         return m_desc
     }
 
+    /**
+     * Update the description
+     *
+     * @param desc the new description
+     */
     public fun updateDesc(desc: String) {
         m_desc = desc
     }
 
+    /**
+     * Reset the description
+     */
     public fun resetDesc() {
         m_desc = null
     }
 
+    /**
+     * Add a user assignment
+     *
+     * @param assignment the user
+     */
     public fun addAssignment(assignment: String) {
         m_assignments.add(assignment)
     }
 
+    /**
+     * Remove a user assignment
+     *
+     * @param assignment the user
+     * @return true if the user assignment could be removed, otherwise false
+     */
     public fun removeAssignment(assignment: String) : Boolean {
         return m_assignments.remove(assignment)
     }
 
+    /**
+     * Remove a user assignment
+     *
+     * @param assignmentIDX the user index in the assignment list
+     * @return true if the user assignment could be removed, otherwise false
+     */
     public fun removeAssignment(assignmentIDX: UByte) {
         m_assignments.removeAt(assignmentIDX.toInt())
     }
 
+    /**
+     * Remove all user assignments
+     */
     public fun resetAssignments() {
         m_assignments.clear()
     }
 
+    /**
+     * Add a label
+     *
+     * @param label the label
+     */
     public fun addLabel(label: String) {
         m_labels.add(label)
     }
 
+    /**
+     * Remove a label
+     *
+     * @param label the label
+     */
     public fun removeLabel(label: String) : Boolean {
         return m_labels.remove(label)
     }
 
+    /**
+     * Remove a label
+     *
+     * @param labelIDX the label index in the label list
+     */
     public fun removeLabel(labelIDX: UByte) {
         m_labels.removeAt(labelIDX.toInt())
     }
 
+    /**
+     * Remove all labels
+     */
     public fun resetLabels() {
         m_labels.clear()
     }
