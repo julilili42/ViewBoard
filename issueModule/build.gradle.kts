f682f66dd878188af173be99e54b443a05c31082
ronny <ronny.goettler@student.uni-tuebingen.de>
2025-06-27 22:09:53 +0200

    [config] added missing dependency

--- issueModule/build.gradle.kts
+++ issueModule/build.gradle.kts
@@ -37,6 +37,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(project(":commonModule"))
+    implementation(project(":labelModule"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
