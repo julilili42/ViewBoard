1fb535a8d3544c6825b8c1b8d764cdfef553f546
ronny <ronny.goettler@student.uni-tuebingen.de>
2025-06-27 20:15:24 +0200

    [refactor] labelAPI class updated due to label rework

--- labelModule/src/main/java/com/viewboard/labelModule/LabelAPI.kt
+++ labelModule/src/main/java/com/viewboard/labelModule/LabelAPI.kt
@@ -1,9 +1,11 @@
-package com.example.viewboard.label
+package com.viewboard.labelModule
 
 abstract class LabelAPI () {
     private val m_labels: ArrayList<Label> = ArrayList<Label>()
 
-    public fun add(label: Label) : Boolean {
+    public fun add(name: String, account: String) : Boolean {
+        val label: Label = Label(name, account)
+
         if (flushAddIMPL(label)) {
             m_labels.add(label)
             return true
@@ -27,6 +29,14 @@ abstract class LabelAPI () {
         m_labels.addAll(labels)
 
         return labels.size.toUInt()
+    }
+
+    public fun getLabel(idx: Int) : Label {
+        return m_labels.get(idx)
+    }
+
+    public fun getLabels() : ArrayList<Label> {
+        return m_labels
     }
 
     protected abstract fun trackIMPL(count: UInt) : ArrayList<Label>
