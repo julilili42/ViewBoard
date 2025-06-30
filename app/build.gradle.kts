d1b5e583001fc3ea781efb9ab4832082e5f22c2c
ronny <ronny.goettler@student.uni-tuebingen.de>
2025-06-30 20:57:23 +0200

    [config] added APIModule

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,7 @@ dependencies {
     implementation(project(":labelModule"))
     implementation(project(":projectModule"))
     implementation(project(":issueModule"))
+    implementation(project(":APIModule"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
