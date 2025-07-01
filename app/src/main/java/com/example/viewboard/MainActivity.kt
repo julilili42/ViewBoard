dc0b835c1515da0e39816d1388ba0372f8840af0
ronny <ronny.goettler@student.uni-tuebingen.de>
2025-07-02 00:49:22 +0200

    for i2aoul

--- app/src/main/java/com/example/viewboard/MainActivity.kt
+++ app/src/main/java/com/example/viewboard/MainActivity.kt
@@ -4,16 +4,80 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.lifecycle.lifecycleScope
+import com.example.viewboard.backend.dataLayout.IssueLayout
+import com.example.viewboard.backend.dataLayout.LabelLayout
+import com.example.viewboard.backend.dataLayout.ProjectLayout
+import com.example.viewboard.backend.dataLayout.ViewLayout
+import com.example.viewboard.backend.storageServer.impl.FirebaseAPI
 import com.example.viewboard.ui.navigation.Navigation
 import com.example.viewboard.ui.theme.ComposeLoginScreenInitTheme
-import java.util.logging.Handler
-import android.content.Intent
-import android.os.Looper
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        FirebaseAPI.init()
+
         enableEdgeToEdge()
+
+        val l = LabelLayout(name = "3. label", creator = "ich")
+//        FirebaseAPI.addLabel(l)
+
+        val i = IssueLayout(title = "3. issue", creator = "ich")
+//        FirebaseAPI.addIssue(i)
+
+        val p = ProjectLayout(name = "first project", creator = "ich", issues = arrayListOf("SlIsrElzBCUuNoPG3G7K", "OplQrgTrggRIW9yDNQ8a", "f7DmLeYkwfQ7IkA6tze3"))
+//        FirebaseAPI.addProject(p)
+
+        lifecycleScope.launch {
+            val p = FirebaseAPI.getProject("ysZaMVY24jnSyLuE5FKJ")
+            println(p!!.creationTS.getFull())
+            p!!.issues.add("SlIsrElzBCUuNoPG3G7K")
+            p!!.issues.add("OplQrgTrggRIW9yDNQ8a")
+            p!!.issues.add("f7DmLeYkwfQ7IkA6tze3")
+//            FirebaseAPI.updProject("ysZaMVY24jnSyLuE5FKJ", p)
+        }
+
+        val v = ViewLayout(name = "first view", creator = "ich", issues = arrayListOf("SlIsrElzBCUuNoPG3G7K"))
+//        FirebaseAPI.addView(v)
+
+        lifecycleScope.launch {
+            val labels = FirebaseAPI.getLabels()
+            labels.collect { list ->
+                list.forEach { i -> println(i) }
+            }
+        }
+
+        lifecycleScope.launch {
+          val issues = FirebaseAPI.getIssues()
+            issues.collect { list ->
+                list.forEach { i -> println(i) }
+            }
+        }
+
+        lifecycleScope.launch {
+            val projects = FirebaseAPI.getProjects()
+            projects.collect { list ->
+                list.forEach { i -> println(i) }
+            }
+        }
+
+        lifecycleScope.launch {
+            val views = FirebaseAPI.getViews()
+            views.collect { list ->
+                list.forEach { i -> println(i) }
+            }
+        }
+
+        lifecycleScope.launch {
+            val views = FirebaseAPI.getIssues("ysZaMVY24jnSyLuE5FKJ")
+            views.collect { list ->
+                list.forEach { i -> println(i) }
+            }
+        }
+
         setContent {
             ComposeLoginScreenInitTheme {
                 Navigation()
