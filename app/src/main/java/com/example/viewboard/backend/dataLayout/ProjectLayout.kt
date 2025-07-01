dc4733374d9b657f5a6185585333b6991b4a120e
ronny <ronny.goettler@student.uni-tuebingen.de>
2025-07-01 22:27:58 +0200

    [feature] added views to ProjectLayout

--- app/src/main/java/com/example/viewboard/backend/dataLayout/ProjectLayout.kt
+++ app/src/main/java/com/example/viewboard/backend/dataLayout/ProjectLayout.kt
@@ -16,6 +16,7 @@ data class ProjectLayout (
     var completedMilestones: Float = 0f,
     var issues: ArrayList<String> = ArrayList<String>(),
     var labels: ArrayList<String> = ArrayList<String>(),
+    var views: ArrayList<String> = ArrayList<String>(),
     var users: ArrayList<String> = ArrayList<String>(),
     var creationTS: Timestamp = Timestamp()
 )
