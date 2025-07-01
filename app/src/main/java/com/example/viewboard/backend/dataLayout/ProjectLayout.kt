e1808fd9f4e29c3ff87db53f388196a644375a28
ronny <ronny.goettler@student.uni-tuebingen.de>
2025-07-01 18:44:13 +0200

    [feature] added ProjectLayout class as data layout

---
+++ app/src/main/java/com/example/viewboard/backend/dataLayout/ProjectLayout.kt
@@ -0,0 +1,16 @@
+package com.example.viewboard.backend.dataLayout
+
+import com.google.firebase.firestore.DocumentId
+
+data class ProjectLayout (
+    @DocumentId
+    var id: String = "",
+    var name: String = "",
+    var desc: String = "",
+    var creator: String = "",
+    var phase: String = "",
+    var startMonth: Int = 0, // 1–12
+    var endMonth: Int = 0,   // 1–12
+    var totalMilestones: Int = 0,
+    var completedMilestones: Float = 0f
+)
