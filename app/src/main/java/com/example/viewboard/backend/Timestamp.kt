cc405101b26f14876a3b24ff3b2a51f9990cacd0
ronny <ronny.goettler@student.uni-tuebingen.de>
2025-07-11 20:23:17 +0200

    [feature] Timestamp construction extended

--- app/src/main/java/com/example/viewboard/backend/Timestamp.kt
+++ app/src/main/java/com/example/viewboard/backend/Timestamp.kt
@@ -8,9 +8,10 @@ class Timestamp (
     dateFmt: String = "dd.MM.yy",
     timeFmt: String = "HH:mm",
     fullFmt: String = "dd.MM.yy HH:mm",
-    zone: ZoneId = ZoneId.systemDefault()
+    zone: ZoneId = ZoneId.systemDefault(),
+    data: String? = null
 ) {
-    private var m_instant: Instant = Instant.now()
+    private var m_instant: Instant = data?.let(Instant::parse) ?: Instant.now()
     private val m_dateFmt: DateTimeFormatter = DateTimeFormatter.ofPattern(dateFmt).withZone(zone)
     private val m_timeFmt: DateTimeFormatter = DateTimeFormatter.ofPattern(timeFmt).withZone(zone)
     private val m_fullFmt: DateTimeFormatter = DateTimeFormatter.ofPattern(fullFmt).withZone(zone)
