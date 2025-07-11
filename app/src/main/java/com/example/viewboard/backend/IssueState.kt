91aafcd117265c836eebe9d91124984e56015a9f
ronny <ronny.goettler@student.uni-tuebingen.de>
2025-07-11 16:09:55 +0200

    [refactor] changed type of label in IssueLayout, moved IssueState into IssueLayout.kt

--- app/src/main/java/com/example/viewboard/backend/IssueState.kt
+++
@@ -1,7 +0,0 @@
-package com.example.viewboard.backend
-
-enum class IssueState {
-    NEW,
-    ONGOING,
-    DONE
-}
--- app/src/main/java/com/example/viewboard/backend/dataLayout/IssueLayout.kt
+++ app/src/main/java/com/example/viewboard/backend/dataLayout/IssueLayout.kt
@@ -2,7 +2,12 @@ package com.example.viewboard.backend.dataLayout
 
 import com.google.firebase.firestore.DocumentId
 import com.example.viewboard.backend.Timestamp
-import com.example.viewboard.backend.IssueState
+
+enum class IssueState {
+    NEW,
+    ONGOING,
+    DONE
+}
 
 data class IssueLayout (
     @DocumentId
@@ -12,7 +17,7 @@ data class IssueLayout (
     var creator: String = "",
     var state: IssueState = IssueState.NEW,
     var assignments: ArrayList<String> = ArrayList<String>(),
-    var labels: ArrayList<LabelLayout> = ArrayList<LabelLayout>(),
+    var labels: ArrayList<String> = ArrayList<String>(),
     var creationTS: Timestamp = Timestamp(),
     var deadlineTS: Timestamp = Timestamp()
 )
