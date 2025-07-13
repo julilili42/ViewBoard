c1c0ea78f3d14e3c4763a51d1607198edb669ca6
ronny <ronny.goettler@student.uni-tuebingen.de>
2025-07-14 00:22:55 +0200

    [refactor] clean main

--- app/src/main/java/com/example/viewboard/MainActivity.kt
+++ app/src/main/java/com/example/viewboard/MainActivity.kt
@@ -4,34 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.lifecycle.lifecycleScope
-import com.example.viewboard.backend.auth.impl.AuthAPI
-import com.example.viewboard.backend.dataLayout.ProjectLayout
 import com.example.viewboard.backend.storageServer.impl.FirebaseAPI
 import com.example.viewboard.ui.navigation.Navigation
 import com.example.viewboard.ui.theme.ComposeLoginScreenInitTheme
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         FirebaseAPI.init()
+
         enableEdgeToEdge()
-
-
-        lifecycleScope.launch {
-            val projects = FirebaseAPI.getProjectsFromUser(AuthAPI.getUid())
-            projects.collect { list ->
-                list.forEach { i -> println(i) }
-            }
-        }
-
-        lifecycleScope.launch {
-            val projects = FirebaseAPI.getAllProjects()
-            projects.collect { list ->
-                list.forEach { i -> println(i) }
-            }
-        }
 
         setContent {
             ComposeLoginScreenInitTheme {
