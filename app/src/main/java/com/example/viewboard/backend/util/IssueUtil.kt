a0acbc0e2fefcc12803613c6ae67f827ddbc02cc
ronny <ronny.goettler@student.uni-tuebingen.de>
2025-07-18 14:09:15 +0200

    [feature] added filterIssuesByStates

--- app/src/main/java/com/example/viewboard/backend/util/IssueUtil.kt
+++ app/src/main/java/com/example/viewboard/backend/util/IssueUtil.kt
@@ -1,6 +1,8 @@
 package com.example.viewboard.backend.util
 
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import com.example.viewboard.backend.dataLayout.IssueLayout
+import com.example.viewboard.backend.dataLayout.IssueState
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
@@ -45,6 +47,21 @@ fun filterIssuesByAssignments(issues: Flow<List<IssueLayout>>, assignments: List
 fun filterIssuesByCreators(issues: Flow<List<IssueLayout>>, creators: List<String>) : Flow<List<IssueLayout>> {
     return issues.map { issue ->
         issue.filter { it.creator in creators }
+            .ifEmpty { emptyList() }
+    }
+}
+
+/**
+ * Get all issues filtered by a set of states
+ *
+ * @param issues the issues to be filtered
+ * @param states the set of states to be used for filtering
+ *
+ * @return the issues when they have been successfully filtered
+ */
+fun filterIssuesByStates(issues: Flow<List<IssueLayout>>, states: List<IssueState>) : Flow<List<IssueLayout>> {
+    return issues.map { issue ->
+        issue.filter { it.state in states }
             .ifEmpty { emptyList() }
     }
 }
