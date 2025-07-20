c7e1642506784e4ef172ab3cd7135a394091b76d
julilili42 <116183344+julilili42@users.noreply.github.com>
2025-07-20 10:58:01 +0200

    [feature] Added new icon and changed name of app

--- app/src/main/java/com/example/viewboard/MainActivity.kt
+++ app/src/main/java/com/example/viewboard/MainActivity.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 //import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.lifecycleScope
 import com.example.viewboard.backend.NotificationHelper
@@ -17,7 +18,7 @@ import kotlinx.coroutines.launch
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       // installSplashScreen()
+        installSplashScreen()
         FirebaseAPI.init()
         enableEdgeToEdge()
         AuthAPI.ensureUserProfileExists(
